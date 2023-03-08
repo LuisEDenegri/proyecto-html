eb4e4931ad37f8a73f9daacf2be220244dfdd446
Luis Denegri <luisdenegri3@gmail.com>
2023-03-08 19:28:41 -0300

    Se agrega el proyecto

---
+++ codigo.js
@@ -0,0 +1,14 @@
+function alerta1(){
+    swal({
+        title: "Pedir prestado el codigo",
+        icon: "success",
+        button: ";)"
+      });
+}
+function alerta2(){
+    swal({
+        title: "Intente con otro navegador",
+        text: "Google Chrome, Mozilla FireFox, Microsoft Edge y Safari",
+        icon: "info",
+      });
+}
